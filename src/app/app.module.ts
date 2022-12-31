6d9a1966e1c219c997728b5679688b12986460ed
Kalebe Nascimento <kalebe.nascimento19@gmail.com>
2022-12-31 07:11:28 -0300

    Feita uma nova divisão de pastas e adicionada o novo componente search-bar a ela

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
+import { HomeComponent } from './components/home-page/home/home.component';
 import { PokeballLoadingComponent } from './components/pokeball-loading/pokeball-loading.component';
 import { PokemonDetailsComponent } from './components/pokemon-details/pokemon-details.component';
+import { SearchBarComponent } from './components/home-page/search-bar/search-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     PokeballLoadingComponent,
-    PokemonDetailsComponent
+    PokemonDetailsComponent,
+    SearchBarComponent
   ],
   imports: [
     BrowserModule,
