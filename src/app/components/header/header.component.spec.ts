27b2e84ba3870341b85d4c2b814ec22735e8a6de
Kalebe Nascimento <kalebe.nascimento19@gmail.com>
2023-01-07 20:16:57 -0300

    Bug fix alguns testes não passavam

--- src/app/components/header/header.component.spec.ts
+++ src/app/components/header/header.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
+import { RouterModule } from '@angular/router';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
@@ -8,7 +10,9 @@ describe('HeaderComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
+      declarations: [ HeaderComponent ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      providers: [RouterModule]
     })
     .compileComponents();
 
